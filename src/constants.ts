1aa394bf5c294c674951a8fdd040e146db2b6e18
Ayrlin <39847734+Ayrlin-Renata@users.noreply.github.com>
2024-10-20 19:45:21 -0500

    WE'RE COOKING

--- src/constants.ts
+++ src/constants.ts
@@ -2,11 +2,14 @@ import "dotenv/config";
 
 export const DID = process.env.DID ?? "";
 export const SIGN_KEY = process.env.SIGN_KEY ?? "";
+export const PASSWORD = process.env.PASSWORD ?? "";
+
 export const PORT = Number(process.env.PORT ?? 4001);
 export const URL = process.env.URL ?? "wss://jetstream.atproto.tools/subscribe";
-export const MAXLABELS = 4;
 
-export const DELETE = "3kwsqucto3j2a"; //message id 
+
+export const MAXLABELS = 4;
+export const DELETE = "3l6ybuq3g3d2v"; //message id 
 export const POSTS: Record<string, string> = {
-    "": "holofan" //message id
+    "3l6yboclrty2l": "holofan" //message id
 }
--- src/post.ts
+++ src/post.ts
@@ -1,13 +1,11 @@
-import 'dotenv/config';
-const l_did = process.env.LABELER_DID || "";
-const l_pw = process.env.LABELER_PASSWORD || "";
+import { DID, PASSWORD } from './constants';
 
 import { Bot } from "@skyware/bot";
 
 const bot = new Bot();
 await bot.login({
-	identifier: l_did,
-	password: l_pw,
+	identifier: DID,
+	password: PASSWORD,
 });
 
 const post = await bot.post({ 
